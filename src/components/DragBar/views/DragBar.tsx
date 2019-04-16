9423ed8b5018761ab673ac546b5179681124fc5b
divasatanica <jayhebes@vip.qq.com>
2019-04-16 23:30:27 +0800

    update: add optional width prop to DragBar component

--- src/components/DragBar/views/DragBar.tsx
+++ src/components/DragBar/views/DragBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.less';
 
-export default ({height}: { height: number}) => {
+export default ({height, width}: { height: number, width?: string }) => {
     return <section className="App-DragBar"
-                    style={{height: height + 'px'}}></section>
+                    style={{height: height + 'px', width}}></section>
 }
